0d43e053b661ef2764e17c9f20d382daf95aa1c0
Elliot Stokes <elliot.stokes@gmail.com>
2022-09-12 14:13:02 +0100

    Update theme to show alerts correctly

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material";
+import { common } from "@mui/material/colors";
 
 const fontWeight = {
   light: 300,
@@ -70,6 +71,125 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           textTransform: "none",
+        },
+      },
+    },
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          padding: "0px",
+          borderRadius: "none",
+        },
+        message: {
+          width: "100%",
+          padding: ".75rem 16px",
+        },
+        icon: {
+          padding: "0.75rem 8px 0.75rem 8px",
+          marginRight: 0,
+        },
+        standardError: {
+          "& > .MuiAlert-message": {
+            color: common.black,
+            backgroundColor: "#FCEBE9",
+            "& > p": {
+              marginTop: "0.35rem",
+            },
+          },
+          "& > .MuiAlert-icon": {
+            color: common.black,
+            backgroundColor: "#DD3327",
+            fontSize: "24px",
+          },
+          "& > .MuiAlert-action": {
+            padding: "12px 16px 0px 16px",
+            marginRight: 0,
+            "& > .MuiButtonBase-root.MuiIconButton-root": {
+              padding: 0,
+              "& > .MuiSvgIcon-root": {
+                fontSize: "24px",
+              },
+            },
+          },
+        },
+        standardWarning: {
+          backgroundColor: "#FFEEBA",
+          "& > .MuiAlert-message": {
+            color: common.black,
+            "& > p": {
+              marginTop: "0.35rem",
+            },
+          },
+          "& > .MuiAlert-icon": {
+            color: common.black,
+            backgroundColor: "#FFC107",
+            fontSize: "24px",
+          },
+          "& > .MuiAlert-action": {
+            padding: "12px 16px 0px 16px",
+            marginRight: 0,
+            "& > .MuiButtonBase-root.MuiIconButton-root": {
+              padding: 0,
+              "& > .MuiSvgIcon-root": {
+                fontSize: "24px",
+              },
+            },
+          },
+        },
+        standardInfo: {
+          backgroundColor: "#E6F6FE",
+          "& > .MuiAlert-message": {
+            color: common.black,
+            "& > p": {
+              marginBottom: "8px",
+            },
+          },
+          "& > .MuiAlert-icon": {
+            color: common.black,
+            backgroundColor: "#03A9F4",
+            fontSize: "24px",
+          },
+          "& > .MuiAlert-action": {
+            padding: "12px 16px 0px 16px",
+            marginRight: 0,
+            "& > .MuiButtonBase-root.MuiIconButton-root": {
+              padding: 0,
+              "& > .MuiSvgIcon-root": {
+                fontSize: "24px",
+              },
+            },
+          },
+        },
+        standardSuccess: {
+          backgroundColor: "#CDE9CE",
+          "& > .MuiAlert-message": {
+            color: common.black,
+            "& > p": {
+              marginTop: "0.35rem",
+            },
+          },
+          "& > .MuiAlert-icon": {
+            color: common.black,
+            backgroundColor: "#4CAF50",
+            fontSize: "24px",
+          },
+          "& > .MuiAlert-action": {
+            padding: "12px 16px 0px 16px",
+            marginRight: 0,
+            "& > .MuiButtonBase-root.MuiIconButton-root": {
+              padding: 0,
+              "& > .MuiSvgIcon-root": {
+                fontSize: "24px",
+              },
+            },
+          },
+        },
+      },
+    },
+    MuiAlertTitle: {
+      styleOverrides: {
+        root: {
+          margin: 0,
         },
       },
     },
