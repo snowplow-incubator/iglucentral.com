8db99f44e6c14a0f481cb4a1fa66903990ec6967
Elliot Stokes <Elliot.stokes@gmail.com>
2023-05-02 11:41:37 +0100

    add extra copy cta to schema viewer (#22)

--- src/components/SchemaView.tsx
+++ src/components/SchemaView.tsx
@@ -2,18 +2,28 @@ import { FC } from "react";
 import { Box, Typography } from "@mui/material";
 import { SelfDescribingSchema } from "../data/types";
 import CodePanel from "./CodePanel";
+import Copy from "./Copy";
+import { useTrackInteraction } from "./Snowplow";
 
 type SchemaViewProps = {
   rawSchema: SelfDescribingSchema;
 };
 const SchemaView: FC<SchemaViewProps> = ({ rawSchema }) => {
+  const trackInteraction = useTrackInteraction();
+  const handleOnCopy = () => {
+    trackInteraction("click", "button", `${rawSchema.self.name}-copy`);
+  };
+  const schemaText = JSON.stringify(rawSchema, null, 2);
   return (
     <Box>
-      <Typography mb={2} variant="h3semibold">
-        JSON Schema
-      </Typography>
+      <Box mb={2} display={"flex"} alignItems={"center"} columnGap={1}>
+        <Typography variant="h3semibold">JSON Schema</Typography>
+        <Copy onClick={handleOnCopy} text={schemaText}>
+          [Copy schema to clipboard]
+        </Copy>
+      </Box>
 
-      <CodePanel language={"json"} code={JSON.stringify(rawSchema, null, 2)} />
+      <CodePanel onCopy={handleOnCopy} language={"json"} code={schemaText} />
     </Box>
   );
 };
